ef3f2ed8e2f40cc192c295f4130291593808abcd
Francisco López <flopez_1384@hotmail.com>
2021-02-22 13:47:45 -0600

    [ADD] The POP method to the array

--- array/array.js
+++ array/array.js
@@ -17,11 +17,20 @@ class MyArray {
     this.length++;
     return this.data;
   }
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
 }
 
 const myArray = new MyArray();
 
 myArray.push("Sebastian");
 myArray.push("Brenda");
+myArray.push("Natalia");
 console.log(myArray);
 console.log(`Obtener el indice 1 de mi arreglo, el valor es ${myArray.get(1)}`);
+myArray.pop();
+console.log("El nuevo arreglo con POP es", myArray);
