eae7fadae59c46f022056bead5021a9c727a45c9
Francisco López <flopez_1384@hotmail.com>
2021-03-02 06:08:35 -0600

    [ADD] The Hash Table and the set method

---
+++ hashTable/hashtable.js
@@ -0,0 +1,36 @@
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+  hashMethod(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+  set(key, value) {
+    const address = this.hashMethod(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+  }
+}
+
+const myHashTable = new HashTable(50);
+
+console.log(myHashTable);
+
+myHashTable.set("Paco", 1984);
+
+console.log(myHashTable);
+
+myHashTable.set("Brenda", 1990);
+
+console.log(myHashTable);
+
+myHashTable.set("Sebastian", 2015);
+
+console.log(myHashTable);
