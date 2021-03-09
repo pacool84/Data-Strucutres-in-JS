8b5186a7fffc1a49b8720584bccb2a84a7250125
Francisco López <flopez_1384@hotmail.com>
2021-03-08 19:46:24 -0600

    [ADD] Linked list data structure

---
+++ LinkedList/single.js
@@ -0,0 +1,33 @@
+let singlyLinkedList = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null,
+      },
+    },
+  },
+};
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class MySinglyLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+}
+
+let MyLinkedList = new MySinglyLinkedList(1);
