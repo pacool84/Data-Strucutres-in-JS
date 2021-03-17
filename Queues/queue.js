546fb2f9cba1326191843b2ee6b9138743438538
Francisco López <flopez_1384@hotmail.com>
2021-03-17 07:42:50 -0600

    [ADD] The queue data structure enqueue/peek

---
+++ Queues/queue.js
@@ -0,0 +1,38 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.first;
+  }
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+
+    return this;
+  }
+}
+
+const myQueue = new Queue();
+myQueue.enqueue(12);
+myQueue.enqueue(3);
+myQueue.enqueue(5);
+myQueue.peek();
+console.log(myQueue);
+console.log(myQueue.peek());
